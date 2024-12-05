e8b9560e3b1d2f8cb2d791ec38a475565056a45b
Sylvia Chapman Kent <124678872+rayne-fall@users.noreply.github.com>
2024-12-05 21:40:41 +0000

    task 7

--- weather.sh
+++ weather.sh
@@ -1,1 +1,5 @@
+#! /bin/bash
+
+echo "Downloading data from Athenry weather station"
 wget -O date +"%Y%m%d_%H%M%S".json https://prodapi.metweb.ie/observations/athenry/today
+echo "Download complete"
